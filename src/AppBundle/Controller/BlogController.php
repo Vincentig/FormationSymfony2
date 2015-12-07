143156cfc9a5b5af1cc4f85e4d4a711a8fe2d6c9
vincentig <blackholus@hotmail.com>
2015-12-07 16:25:29 +0100

    fin journée 1 symfony

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -6,19 +6,63 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * prefix blog
+ * @Route("/blog")
+ */
 class BlogController extends Controller {
 
     /**
-     * @Route("/blog/{page}",
+     * @Route("/{page}",
      *      name="blog_homepage", 
      *      defaults={"page":1}, 
      *      requirements={"page": "\d+"})
      */
     public function indexAction($page) {
-        // replace this example code with whatever you need
+
+        $articles = [
+            ['titre' => 'Le titre', 'contenu' => 'Blablabla <b>bliblibli</b>'],
+            ['titre' => 'L\'autre titre', 'contenu' => 'le contenu de l\autre article'],
+            ['titre' => 'Le dernier titre', 'contenu' => 'le  dernier contenu du dernier article']
+        ];
+
         return $this->render('blog/index.html.twig', array(
-                    'message' => 'Coucou là-dedans',
-                    'page' => $page,
+                    'articles' => $articles,
+                    'page' => $page
+        ));
+    }
+
+    /**
+     * @Route("/article/{id}",
+     *      name="blog_article", 
+     *      requirements={"id": "\d+"})
+     */
+    public function articleAction($id) {
+        // replace this example code with whatever you need
+        return $this->render('blog/article.html.twig', array(
+                    'id' => $id
+        ));
+    }
+
+    /**
+     * @Route("/admin/ajouter",
+     *      name="blog_ajouter")
+     */
+    public function ajouterAction() {
+        // replace this example code with whatever you need
+        return $this->render('blog/ajouter.html.twig', array(
+        ));
+    }
+
+    /**
+     * @Route("/admin/modifier/{id}",
+     *      name="blog_article", 
+     *      requirements={"id": "\d+"})
+     */
+    public function modifierAction($id) {
+        // replace this example code with whatever you need
+        return $this->render('blog/modifier.html.twig', array(
+                    'id' => $id
         ));
     }
 
