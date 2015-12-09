f4e3f454de1cd07f1e108109d990795451048315
vincentig <blackholus@hotmail.com>
2015-12-09 16:22:51 +0100

    fin du mercredi 09/12/2015

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -22,8 +22,9 @@ class BlogController extends Controller {
     public function indexAction($page) {
 
         $articleManager = $this->getDoctrine()->getManager()->getRepository('AppBundle:Article');
-        $articles = $articleManager->findAll();
-
+//        $articles = $articleManager->findAll();
+        // $articles = $articleManager->findBy(array('publication' => true), array('date' => 'desc'), null, null);
+        $articles = $articleManager->getArticles();
 
 //        $articles = [
 //            ['id' => 1, 'titre' => 'Le titre', 'contenu' => 'Blablabla <b>bliblibli</b>'],
@@ -46,12 +47,13 @@ class BlogController extends Controller {
         $articleManager = $this->getDoctrine()->getManager()->getRepository('AppBundle:Article');
         $article = $articleManager->find($id);
 
-        $commentaireManager = $this->getDoctrine()->getManager()->getRepository('AppBundle:Commentaire');
-        $commentaires = $commentaireManager->findBy(array('article' => $article));
+//        $commentaireManager = $this->getDoctrine()->getManager()->getRepository('AppBundle:Commentaire');
+//        $commentaires = $commentaireManager->findBy(
+//                array('article' => $article), array('date' => 'desc'));
         // replace this example code with whatever you need
         return $this->render('blog/article.html.twig', array(
                     'article' => $article,
-                    'commentaires' => $commentaires
+//                    'commentaires' => $commentaires
         ));
     }
 
@@ -62,10 +64,10 @@ class BlogController extends Controller {
     public function ajouterAction() {
 
         $article = new Article();
-        $article->setTitre('Le nouvel article créé depuis le controleur')->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu sem sem. Suspendisse maximus pellentesque diam vitae vehicula. Ut rutrum nibh elit, sit amet interdum orci ultrices quis. Pellentesque vel congue turpis, eget rhoncus lacus. Nullam mollis lectus at tellus maximus commodo. Aenean nisl dui, bibendum quis enim in, vehicula consectetur dolor. Quisque iaculis ex dolor, ac condimentum dui rhoncus at. Ut condimentum felis lorem, et mattis orci euismod eget. Donec massa sapien, egestas a augue tincidunt, ultrices volutpat nisi. Ut iaculis luctus libero ac commodo. Nunc nisi augue, efficitur nec odio sed, molestie pulvinar massa. Aliquam vulputate gravida ligula eu aliquam. Vivamus a eros maximus, fermentum est ut, vestibulum diam. Nulla fermentum a est sit amet laoreet.
-
-Mauris vitae augue vitae augue mollis vulputate ac ut neque. Donec at varius urna. Cras suscipit felis ipsum, sit amet auctor ipsum aliquet in. Aliquam a libero quis sapien blandit tristique ac nec mi. Pellentesque facilisis magna tortor, a sollicitudin ipsum pharetra ut. Duis bibendum tincidunt metus nec vehicula. Mauris finibus lectus vitae tortor egestas commodo. Curabitur condimentum elit quis lorem ornare, rutrum placerat elit laoreet. Praesent volutpat dui sed molestie viverra. Nunc mollis lacinia felis, quis ullamcorper elit dictum id. Pellentesque fringilla commodo nulla, consequat luctus lectus ullamcorper ac. Nullam lacinia purus sed nibh accumsan, quis aliquet ex bibendum.
-
+        $article->setTitre('Le nouvel article créé depuis le controleur')->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu sem sem. Suspendisse maximus pellentesque diam vitae vehicula. Ut rutrum nibh elit, sit amet interdum orci ultrices quis. Pellentesque vel congue turpis, eget rhoncus lacus. Nullam mollis lectus at tellus maximus commodo. Aenean nisl dui, bibendum quis enim in, vehicula consectetur dolor. Quisque iaculis ex dolor, ac condimentum dui rhoncus at. Ut condimentum felis lorem, et mattis orci euismod eget. Donec massa sapien, egestas a augue tincidunt, ultrices volutpat nisi. Ut iaculis luctus libero ac commodo. Nunc nisi augue, efficitur nec odio sed, molestie pulvinar massa. Aliquam vulputate gravida ligula eu aliquam. Vivamus a eros maximus, fermentum est ut, vestibulum diam. Nulla fermentum a est sit amet laoreet.<br/>
+<br/>
+Mauris vitae augue vitae augue mollis vulputate ac ut neque. Donec at varius urna. Cras suscipit felis ipsum, sit amet auctor ipsum aliquet in. Aliquam a libero quis sapien blandit tristique ac nec mi. Pellentesque facilisis magna tortor, a sollicitudin ipsum pharetra ut. Duis bibendum tincidunt metus nec vehicula. Mauris finibus lectus vitae tortor egestas commodo. Curabitur condimentum elit quis lorem ornare, rutrum placerat elit laoreet. Praesent volutpat dui sed molestie viverra. Nunc mollis lacinia felis, quis ullamcorper elit dictum id. Pellentesque fringilla commodo nulla, consequat luctus lectus ullamcorper ac. Nullam lacinia purus sed nibh accumsan, quis aliquet ex bibendum.<br/>
+<br/>
 Ut eu ex euismod, vehicula mi nec, porta arcu. Fusce cursus porta lacinia. Vestibulum sollicitudin, sapien eu efficitur dignissim, lorem purus rhoncus eros, vel tincidunt sem enim egestas ante. Nullam lacinia rutrum leo. Fusce pretium risus vel justo fringilla sodales a vel lorem. Vivamus dignissim, diam non maximus egestas, lectus magna commodo lectus, vel varius ligula orci eget justo. Morbi commodo turpis ut condimentum volutpat. Praesent vel tempor leo. Pellentesque sed dapibus lacus. Morbi vulputate tortor ut lorem vehicula molestie sit amet sit amet dolor. Duis feugiat convallis tortor eget auctor. ')
                 ->setAuteur('Vincent');
 
@@ -88,6 +90,24 @@ Ut eu ex euismod, vehicula mi nec, porta arcu. Fusce cursus porta lacinia. Vesti
         $commentaire2->setContenu('génial ça marche');
         $commentaire2->setArticle($article);
 
+        $commentaire3 = new \AppBundle\Entity\Commentaire();
+        $commentaire3->setAuteur('Jean Aymard');
+        $commentaire3->setContenu('Quand y en a marre y a malabard');
+        $article->addCommentaire($commentaire3);
+
+        // en commentaire car unique en base et déjà créés
+        $categorie1 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(1);
+        //      $categorie1 = new \AppBundle\Entity\Categorie();
+        //       $categorie1->setTitre('Chimie');
+        $article->addCategory($categorie1);
+        $categorie2 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(2);
+        //  $categorie2 = new \AppBundle\Entity\Categorie();
+        //      $categorie2->setTitre('Blabla');
+        $article->addCategory($categorie2);
+        $categorie3 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(5);
+        $article->addCategory($categorie3);
+
+
         $manageur->persist($commentaire1);
         $manageur->persist($commentaire2);
         $manageur->persist($article);
@@ -108,8 +128,34 @@ Ut eu ex euismod, vehicula mi nec, porta arcu. Fusce cursus porta lacinia. Vesti
      *      requirements={"id": "\d+"})
      */
     public function modifierAction($id) {
-        // replace this example code with whatever you need
-        return $this->render('blog/modifier.html.twig', array(
+
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository('AppBundle:Article')->find($id);
+        $article->setTitre($article->getTitre() . ' - Modifié');
+        try {
+            $em->flush();
+        } catch (Exception $ex) {
+            echo $ex;
+        }
+
+        /* return $this->render('blog/modifier.html.twig', array(
+          'article' => $article
+          )); */
+        $url = $this->generateUrl('blog_article', array('id' => $id));
+
+        return $this->redirect($url);
+    }
+
+    /**
+     * @Route("/admin/supprimer/{id}",
+     *      name="blog_supprimer", 
+     *      requirements={"id": "\d+"})
+     */
+    public function supprimerAction($id) {
+
+
+
+        return $this->render('blog/supprimer.html.twig', array(
                     'id' => $id
         ));
     }
@@ -118,14 +164,34 @@ Ut eu ex euismod, vehicula mi nec, porta arcu. Fusce cursus porta lacinia. Vesti
      * 
      */
     public function menuGaucheAction($nombre) {
-        $articles = [
-            ['id' => 1, 'titre' => 'Le titre', 'contenu' => 'Blablabla <b>bliblibli</b>'],
-            ['id' => 2, 'titre' => 'L\'autre titre', 'contenu' => 'le contenu de l\autre article'],
-            ['id' => 3, 'titre' => 'Le dernier titre', 'contenu' => 'le  dernier contenu du dernier article']
-        ];
+//        $articles = [
+//            ['id' => 1, 'titre' => 'Le titre', 'contenu' => 'Blablabla <b>bliblibli</b>'],
+//            ['id' => 2, 'titre' => 'L\'autre titre', 'contenu' => 'le contenu de l\autre article'],
+//            ['id' => 3, 'titre' => 'Le dernier titre', 'contenu' => 'le  dernier contenu du dernier article']
+//        ];
+
+        $articleManager = $this->getDoctrine()->getManager()->getRepository('AppBundle:Article');
+//        $articles = $articleManager->findAll();
+        $articles = $articleManager->findBy(array(), array('date' => 'desc'), $nombre, 0);
 
         return $this->render('blog/menu_gauche.html.twig', array(
                     'articles' => $articles
+        ));
+    }
+
+    /**
+     * @Route("/categorie/{titre}",
+     *      name="blog_categorie", 
+     *      requirements={"titre": ".+"})
+     */
+    public function categorieAction($titre) {
+        $categorieManager = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie');
+        $categorie = $categorieManager->findOneBy(array('titre' => $titre), null, null, null);
+
+
+        // replace this example code with whatever you need
+        return $this->render('blog/categorie.html.twig', array(
+                    'categorie' => $categorie
         ));
     }
 
