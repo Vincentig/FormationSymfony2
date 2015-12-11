3d717e8cf613f644787fa701feb01a077feec661
vincentig <blackholus@hotmail.com>
2015-12-11 16:20:11 +0100

    Fin de journée vendredi 11/12/2015, commencé formulaires

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -6,6 +6,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use \AppBundle\Entity\Article;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * prefix blog
@@ -73,64 +78,87 @@ class BlogController extends Controller {
      *      name="blog_ajouter")
      */
     public function ajouterAction() {
+        /*
+          $article = new Article();
+          $article->setTitre('Le nouvel article créé depuis le controleur')->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu sem sem. Suspendisse maximus pellentesque diam vitae vehicula. Ut rutrum nibh elit, sit amet interdum orci ultrices quis. Pellentesque vel congue turpis, eget rhoncus lacus. Nullam mollis lectus at tellus maximus commodo. Aenean nisl dui, bibendum quis enim in, vehicula consectetur dolor. Quisque iaculis ex dolor, ac condimentum dui rhoncus at. Ut condimentum felis lorem, et mattis orci euismod eget. Donec massa sapien, egestas a augue tincidunt, ultrices volutpat nisi. Ut iaculis luctus libero ac commodo. Nunc nisi augue, efficitur nec odio sed, molestie pulvinar massa. Aliquam vulputate gravida ligula eu aliquam. Vivamus a eros maximus, fermentum est ut, vestibulum diam. Nulla fermentum a est sit amet laoreet.<br/>
+          <br/>
+          Mauris vitae augue vitae augue mollis vulputate ac ut neque. Donec at varius urna. Cras suscipit felis ipsum, sit amet auctor ipsum aliquet in. Aliquam a libero quis sapien blandit tristique ac nec mi. Pellentesque facilisis magna tortor, a sollicitudin ipsum pharetra ut. Duis bibendum tincidunt metus nec vehicula. Mauris finibus lectus vitae tortor egestas commodo. Curabitur condimentum elit quis lorem ornare, rutrum placerat elit laoreet. Praesent volutpat dui sed molestie viverra. Nunc mollis lacinia felis, quis ullamcorper elit dictum id. Pellentesque fringilla commodo nulla, consequat luctus lectus ullamcorper ac. Nullam lacinia purus sed nibh accumsan, quis aliquet ex bibendum.<br/>
+          <br/>
+          Ut eu ex euismod, vehicula mi nec, porta arcu. Fusce cursus porta lacinia. Vestibulum sollicitudin, sapien eu efficitur dignissim, lorem purus rhoncus eros, vel tincidunt sem enim egestas ante. Nullam lacinia rutrum leo. Fusce pretium risus vel justo fringilla sodales a vel lorem. Vivamus dignissim, diam non maximus egestas, lectus magna commodo lectus, vel varius ligula orci eget justo. Morbi commodo turpis ut condimentum volutpat. Praesent vel tempor leo. Pellentesque sed dapibus lacus. Morbi vulputate tortor ut lorem vehicula molestie sit amet sit amet dolor. Duis feugiat convallis tortor eget auctor. ')
+          ->setAuteur('Vincent');
+
+          $image = new \AppBundle\Entity\Image();
+          $image->setUrl('001.jpg');
+          $image->setAlt('La premiere image');
+
+          $article->setImage($image);
+
+          $doctrine = $this->getDoctrine();
+          $manageur = $doctrine->getManager(); //gère la base de donnée
+
+          $commentaire1 = new \AppBundle\Entity\Commentaire();
+          $commentaire1->setAuteur('vincent');
+          $commentaire1->setContenu('Trop de la balle !');
+          $commentaire1->setArticle($article);
+
+          $commentaire2 = new \AppBundle\Entity\Commentaire();
+          $commentaire2->setAuteur('TotoLeHero');
+          $commentaire2->setContenu('génial ça marche');
+          $commentaire2->setArticle($article);
+
+          $commentaire3 = new \AppBundle\Entity\Commentaire();
+          $commentaire3->setAuteur('Jean Aymard');
+          $commentaire3->setContenu('Quand y en a marre y a malabard');
+          $article->addCommentaire($commentaire3);
+
+          // en commentaire car unique en base et déjà créés
+          $categorie1 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(1);
+          //      $categorie1 = new \AppBundle\Entity\Categorie();
+          //       $categorie1->setTitre('Chimie');
+          $article->addCategory($categorie1);
+          $categorie2 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(2);
+          //  $categorie2 = new \AppBundle\Entity\Categorie();
+          //      $categorie2->setTitre('Blabla');
+          $article->addCategory($categorie2);
+          $categorie3 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(5);
+          $article->addCategory($categorie3);
+
+
+          $manageur->persist($commentaire1);
+          $manageur->persist($commentaire2);
+          $manageur->persist($article);
+          try {
+          $manageur->flush();
+          } catch (Exception $ex) {
+          echo $ex;
+          }
+         */
 
         $article = new Article();
-        $article->setTitre('Le nouvel article créé depuis le controleur')->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu sem sem. Suspendisse maximus pellentesque diam vitae vehicula. Ut rutrum nibh elit, sit amet interdum orci ultrices quis. Pellentesque vel congue turpis, eget rhoncus lacus. Nullam mollis lectus at tellus maximus commodo. Aenean nisl dui, bibendum quis enim in, vehicula consectetur dolor. Quisque iaculis ex dolor, ac condimentum dui rhoncus at. Ut condimentum felis lorem, et mattis orci euismod eget. Donec massa sapien, egestas a augue tincidunt, ultrices volutpat nisi. Ut iaculis luctus libero ac commodo. Nunc nisi augue, efficitur nec odio sed, molestie pulvinar massa. Aliquam vulputate gravida ligula eu aliquam. Vivamus a eros maximus, fermentum est ut, vestibulum diam. Nulla fermentum a est sit amet laoreet.<br/>
-<br/>
-Mauris vitae augue vitae augue mollis vulputate ac ut neque. Donec at varius urna. Cras suscipit felis ipsum, sit amet auctor ipsum aliquet in. Aliquam a libero quis sapien blandit tristique ac nec mi. Pellentesque facilisis magna tortor, a sollicitudin ipsum pharetra ut. Duis bibendum tincidunt metus nec vehicula. Mauris finibus lectus vitae tortor egestas commodo. Curabitur condimentum elit quis lorem ornare, rutrum placerat elit laoreet. Praesent volutpat dui sed molestie viverra. Nunc mollis lacinia felis, quis ullamcorper elit dictum id. Pellentesque fringilla commodo nulla, consequat luctus lectus ullamcorper ac. Nullam lacinia purus sed nibh accumsan, quis aliquet ex bibendum.<br/>
-<br/>
-Ut eu ex euismod, vehicula mi nec, porta arcu. Fusce cursus porta lacinia. Vestibulum sollicitudin, sapien eu efficitur dignissim, lorem purus rhoncus eros, vel tincidunt sem enim egestas ante. Nullam lacinia rutrum leo. Fusce pretium risus vel justo fringilla sodales a vel lorem. Vivamus dignissim, diam non maximus egestas, lectus magna commodo lectus, vel varius ligula orci eget justo. Morbi commodo turpis ut condimentum volutpat. Praesent vel tempor leo. Pellentesque sed dapibus lacus. Morbi vulputate tortor ut lorem vehicula molestie sit amet sit amet dolor. Duis feugiat convallis tortor eget auctor. ')
-                ->setAuteur('Vincent');
+        $formB = $this->createFormBuilder($article);
+        //ancienne methode
+        /*     $formB->add('titre')
+          ->add('contenu', 'textarea')
+          ->add('auteur', 'text')
+          ->add('publication', 'checkbox')
+          ->add('date', 'date')
+          ->add('ok', 'submit');
+         */
+        //nouvelle methode
+        $formB->add('titre', TextType::class)
+                ->add('contenu', TextareaType::class)
+                ->add('auteur', TextType::class)
+                ->add('publication', CheckboxType::class)
+                ->add('date', DateType::class)
+                ->add('ok', SubmitType::class);
 
-        $image = new \AppBundle\Entity\Image();
-        $image->setUrl('001.jpg');
-        $image->setAlt('La premiere image');
-
-        $article->setImage($image);
-
-        $doctrine = $this->getDoctrine();
-        $manageur = $doctrine->getManager(); //gère la base de donnée
-
-        $commentaire1 = new \AppBundle\Entity\Commentaire();
-        $commentaire1->setAuteur('vincent');
-        $commentaire1->setContenu('Trop de la balle !');
-        $commentaire1->setArticle($article);
-
-        $commentaire2 = new \AppBundle\Entity\Commentaire();
-        $commentaire2->setAuteur('TotoLeHero');
-        $commentaire2->setContenu('génial ça marche');
-        $commentaire2->setArticle($article);
-
-        $commentaire3 = new \AppBundle\Entity\Commentaire();
-        $commentaire3->setAuteur('Jean Aymard');
-        $commentaire3->setContenu('Quand y en a marre y a malabard');
-        $article->addCommentaire($commentaire3);
-
-        // en commentaire car unique en base et déjà créés
-        $categorie1 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(1);
-        //      $categorie1 = new \AppBundle\Entity\Categorie();
-        //       $categorie1->setTitre('Chimie');
-        $article->addCategory($categorie1);
-        $categorie2 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(2);
-        //  $categorie2 = new \AppBundle\Entity\Categorie();
-        //      $categorie2->setTitre('Blabla');
-        $article->addCategory($categorie2);
-        $categorie3 = $this->getDoctrine()->getManager()->getRepository('AppBundle:Categorie')->find(5);
-        $article->addCategory($categorie3);
+        $form = $formB->getForm();
 
 
-        $manageur->persist($commentaire1);
-        $manageur->persist($commentaire2);
-        $manageur->persist($article);
-        try {
-            $manageur->flush();
-        } catch (Exception $ex) {
-            echo $ex;
-        }
 
-        $url = $this->generateUrl('blog_article', array('id' => $article->getId()));
-
-        return $this->redirect($url);
+        return $this->render('blog/ajouter.html.twig', array(
+                    'form' => $form->createView()
+        ));
     }
 
     /**
