90d2d4e5e2dd4cbc72433fa84f7d43e797f97894
vincentig <blackholus@hotmail.com>
2015-12-21 16:25:02 +0100

    Fin de journée du 21/12 ajout des utilisateurs avec gestion des droits

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Categorie controller.
+ *
+ * @Route("/admin")
+ */
+class AdminController extends Controller {
+
+    /**
+     * Lists all Categorie entities.
+     *
+     * @Route("/", name="admin_homepage")
+     * @Method("GET")
+     */
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Categorie')->findAll();
+
+
+
+        return $this->render('layout_back.html.twig', array(
+                    'entities' => $entities,
+        ));
+    }
+
+}
